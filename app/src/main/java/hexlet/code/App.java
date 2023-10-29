15f95b16a1202405b18a4f755bcdb274a4b35fa0
Theodor <jeefo93@gmail.com>
2023-10-29 18:03:40 +0300

    Initial release

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,14 +1,7 @@
 package hexlet.code;
 
-import java.util.Random;
-
 public class App {
     public static void main(String[] args) {
-        limRan();
-    }
-
-    private static void limRan() {
-        Random ran = new Random();
-        int i = ran.nextInt(1, 400);
+        System.out.println("Hello World!");
     }
 }
