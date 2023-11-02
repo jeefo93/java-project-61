5f1fa2bb3e38a1fb422bcac8896377b10b88f695
Theodor <jeefo93@gmail.com>
2023-11-02 14:27:11 +0300

    Improves readability; introducing Greet.printList()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,27 +13,34 @@ public class App {
         int greetChoiceNumber = 1;
         int gameListStartNumber = 2;
 
-        Console.writeMessage("Please enter the game number and press Enter.");
-        Console.writeMessage(greetChoiceNumber + " - " + "Greet");
-        for (int optionNumber = gameListStartNumber; optionNumber <= lastAvailableChoiceNumber; optionNumber++) {
-            Console.writeMessage(optionNumber + " - " + gameList[optionNumber - gameListStartNumber]);
-        }
-        Console.writeMessage(exitChoiceNumber + " - " + "Exit");
+        Greet.printList(
+                "Please enter the game number and press Enter.",
+                greetChoiceNumber, "Greet",
+                exitChoiceNumber, "Exit",
+                gameList, gameListStartNumber, lastAvailableChoiceNumber, " - "
+        );
 
         int userChoice = Console.readInt("Your choice: ");
 
         if (userChoice == exitChoiceNumber) {
             Console.writeMessage("Good bye!");
+
         } else if (userChoice < exitChoiceNumber || userChoice > lastAvailableChoiceNumber) {
             Console.writeMessage("There is no game with the given number.");
+
         } else {
-            String userName = Greet.requestUserName();
+            String userName = Greet.requestUserName(
+                    "Welcome to the Brain Games!",
+                    "May I have your name? ",
+                    "Hello, "
+            );
             if (userChoice != greetChoiceNumber) {
                 String gameName = gameList[userChoice - gameListStartNumber];
                 boolean userWon = Invoker.playGame(gameName);
                 String exitMessage = userWon ? "Congratulations, " : "Let's try again, ";
                 Console.writeMessage(exitMessage + userName + "!");
             }
+
         }
     }
 }
--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -3,10 +3,24 @@ package hexlet.code;
 import engine.Console;
 
 public class Greet {
-    public static String requestUserName() {
-        Console.writeMessage("Welcome to the Brain Games!");
-        String userName = Console.readStr("May I have your name? ");
-        Console.writeMessage("Hello, " + userName + "!");
+    public static void printList(String welcomeMsg,
+         int firstNum, String firstName,
+         int lastNum, String lastName,
+         String[] list, int listStartNum, int listEndNum, String separator
+    ) {
+
+        Console.writeMessage(welcomeMsg);
+        Console.writeMessage(firstNum + separator + firstName);
+        for (int optionNumber = listStartNum; optionNumber <= listEndNum; optionNumber++) {
+            Console.writeMessage(optionNumber + separator + list[optionNumber - listStartNum]);
+        }
+        Console.writeMessage(lastNum + separator + lastName);
+    }
+
+    public static String requestUserName(String welcomeMsg, String prompt, String greet) {
+        Console.writeMessage(welcomeMsg);
+        String userName = Console.readStr(prompt);
+        Console.writeMessage(greet + userName + "!");
         return userName;
     }
 }
