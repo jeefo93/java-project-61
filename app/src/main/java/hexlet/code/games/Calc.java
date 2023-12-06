f6c17d5e973452b23115ba18dc4f08ab938576ba
Jeefo <jeefo93@gmail.com>
2023-12-06 03:17:27 +0300

    Calc.java: replaces legacy switch with enhanced

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,31 +3,32 @@ package hexlet.code.games;
 public class Calc {
     public static String[][] generateRoundData(int roundCount) {
         var roundData = new String[roundCount][2];
+
         for (var i = 0; i < roundCount; i++) {
+
             int int1 = (int)(Math.random() * 100);
             int int2 = (int)(Math.random() * 100);
 
             String action;
-            int result;
-            int actionNumber = (int)(Math.random() * 3);
+            int actionNumber = (int)(Math.random() * 2);
 
-            switch (actionNumber) {
-                case 0:
+            int actionResult = switch (actionNumber) {
+                case 0 -> {
                     action = " * ";
-                    result = int1 * int2;
-                    break;
-                case 1:
+                    yield int1 * int2;
+                }
+                case 1 -> {
                     action = " - ";
-                    result = int1 - int2;
-                    break;
-                default:
+                    yield int1 - int2;
+                }
+                default -> {
                     action = " + ";
-                    result = int1 + int2;
-                    break;
-            }
+                    yield int1 + int2;
+                }
+            };
 
             roundData[i][0] = int1 + action + int2;
-            roundData[i][1] = String.valueOf(result);
+            roundData[i][1] = String.valueOf(actionResult);
         }
         return roundData;
     }
