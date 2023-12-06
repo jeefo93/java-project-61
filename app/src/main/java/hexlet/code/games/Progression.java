63be3537c1d66d1fcb3b64071ccf617056040478
Theodor <jeefo93@gmail.com>
2023-12-06 16:52:00 +0300

    Progression.java: Minor refactoring

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -6,22 +6,22 @@ public class Progression {
         String[] progression = new String[2];
         var progressionString = new StringBuilder();
 
-        int rangeLength = (int)((Math.random() * 7) + 5);
-        int currentNumber = (int)(Math.random() * 49);
-        int addendum = (int)((Math.random() * 99) + 1);
-        int emptySlotNumber = (int)(Math.random() * (rangeLength - 1));
+        int progressionLength = (int)((Math.random() * 7) + 5); // progression length is between 5 and 12
+        int currentNumber = (int)(Math.random() * 49); // the first progression member is in 0..49
+        int addendum = (int)((Math.random() * 99) + 1); // addendum is 0..99 + 1 (must not be zero)
+        int emptySlotIndex = (int)(Math.random() * (progressionLength - 1)); // is one of range indexes
 
-        for (int i = 0; i < rangeLength; i++) {
+        for (int i = 0; i < progressionLength; i++) {
             var currentMember = String.valueOf(currentNumber);
 
-            if (i == emptySlotNumber) {
+            if (i == emptySlotIndex) {
                 progression[1] = currentMember;
                 currentMember = "..";
             }
 
             progressionString.append(currentMember);
 
-            if (i < (rangeLength - 1)) {
+            if (i < (progressionLength - 1)) {
                 progressionString.append(" ");
                 currentNumber += addendum;
             }
@@ -32,8 +32,7 @@ public class Progression {
     }
 
     public static String[][] generateRoundData(int roundCount) {
-        String[][] roundData = new String[roundCount][2];
-
+        var roundData = new String[roundCount][2];
         for (int i = 0; i < roundCount; i++) {
             roundData[i] = generateProgression();
         }
