75732d1ca434b0cfd39040e681072a6a467eccb5
Jeefo <jeefo93@gmail.com>
2023-12-06 03:28:23 +0300

    Minor refactoring

--- app/src/main/java/hexlet/code/Invoker.java
+++ app/src/main/java/hexlet/code/Invoker.java
@@ -4,36 +4,29 @@ import hexlet.code.games.*;
 
 public class Invoker {
     public static void playGame(String gameNumber) {
-
         var roundCount = 3;
         var roundData = new String[roundCount][2];
         var rules = "";
 
         switch (gameNumber) {
-
             case "1":
                 rules = "Greet";
                 break;
-
             case "2":
                 rules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
                 roundData = Even.generateRoundData(roundCount);
                 break;
-
             case "3":
                 rules = "What is the result of the expression?";
                 roundData = Calc.generateRoundData(roundCount);
                 break;
-
             case "4":
                 rules = "Find the greatest common divisor of given numbers.";
                 roundData = GCD.generateRoundData(roundCount);
                 break;
-
             case "5":
                 rules = "What number is missing in the progression?";
                 roundData = Progression.generateRoundData(roundCount);
-
             default:
                 break;
         }
