02373a121d8aad68b7f03c3ce0a9478ad63c4ef3
Theodor <jeefo93@gmail.com>
2023-12-08 06:35:58 +0300

    Engine.java: Adds return instead of break

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,38 +3,36 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    public static void run(String[][] roundData, String rules) {
-        var scanner = new Scanner(System.in);
+    public static void run(String[][] roundsData, String rules) {
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        System.out.print("\nWelcome to the Brain Games!\nMay I have your name? ");
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
+            System.out.print("\nWelcome to the Brain Games!\nMay I have your name? ");
+            String userName = scanner.next();
+            System.out.println("Hello, " + userName + "!");
 
-        if (roundData[0][0] == null) {
-            return;
-        }
-
-        System.out.println(rules);
-
-        int lastRoundNum = roundData.length - 1;
-        for (int i = 0; i <= lastRoundNum; i++) {
-
-            System.out.print("Question: " + roundData[i][0] + "\nYour answer: ");
-            String answer = scanner.next();
-
-            if (!answer.equals(roundData[i][1])) {
-                System.out.println("'" + answer + "' "
-                        + "is wrong answer ;(. Correct answer was '" + roundData[i][1] + "'.\n"
-                        + "Let's try again, " + userName + "!");
-                break;
+            if (roundsData[0][0] == null) {
+                return;
             }
 
-            System.out.println("Correct!"
-                               + (i != lastRoundNum ? ""
-                                       : "\nCongratulations, " + userName + "!")
-            );
+            System.out.println(rules);
 
+            int lastRoundNum = roundsData.length - 1;
+            for (int i = 0; i <= lastRoundNum; i++) {
+
+                System.out.print("Question: " + roundsData[i][0] + "\nYour answer: ");
+                String answer = scanner.next();
+
+                if (!answer.equals(roundsData[i][1])) {
+                    System.out.println("'" + answer + "' "
+                            + "is wrong answer ;(. Correct answer was '" + roundsData[i][1] + "'.\n"
+                            + "Let's try again, " + userName + "!");
+                    return;
+                }
+
+                System.out.println("Correct!");
+
+            }
+            System.out.println("Congratulations, " + userName + "!");
         }
-        scanner.close();
     }
 }
