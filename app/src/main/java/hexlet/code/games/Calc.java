87aa4b45f7ca28fc9d7b4f8104b69d0992e34f66
Theodor <jeefo93@gmail.com>
2023-12-08 07:22:12 +0300

    Calc.java: moves calculation to dedicated method

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,34 +5,29 @@ import hexlet.code.Utils;
 public class Calc {
 
     private static final int MAX_INT_VALUE = 100;
-    private static final int MAX_ACTION_NUMBER = 2;
+    private static final String[] ACTIONS = {"+", "-", "*"};
+
+    private static int getActionResult(int intA, int intB, String actionLabel) {
+        return switch (actionLabel) {
+            case "+" -> intA + intB;
+            case "-" -> intA - intB;
+            default -> intA * intB;
+        };
+    }
 
     public static String[][] generateRoundData(int roundCount) {
         var roundData = new String[roundCount][2];
-        String action;
 
         for (var i = 0; i < roundCount; i++) {
 
             int intA = Utils.getRandomInt(0, MAX_INT_VALUE);
             int intB = Utils.getRandomInt(0, MAX_INT_VALUE);
-            int actionNumber = Utils.getRandomInt(0, MAX_ACTION_NUMBER);
 
-            int actionResult = switch (actionNumber) {
-                case 0 -> {
-                    action = " + ";
-                    yield intA + intB;
-                }
-                case 1 -> {
-                    action = " - ";
-                    yield intA - intB;
-                }
-                default -> {
-                    action = " * ";
-                    yield intA * intB;
-                }
-            };
+            int actionNumber = Utils.getRandomInt(0, (ACTIONS.length - 1));
+            var actionLabel = ACTIONS[actionNumber];
+            int actionResult = getActionResult(intA, intB, actionLabel);
 
-            roundData[i][0] = intA + action + intB;
+            roundData[i][0] = intA + " " + actionLabel + " " + intB;
             roundData[i][1] = String.valueOf(actionResult);
 
         }
